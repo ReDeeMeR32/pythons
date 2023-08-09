2ee0510cd47ff1f3ecabf456c882d130df4d530c
ReDeeMeR32 <danilka2002bb@list.ru>
2023-08-09 21:27:02 +0300

    commited

---
+++ fileForTries.py
@@ -0,0 +1,3 @@
+from extendedIntegerFunc import expanded_form
+
+print(expanded_form(1323))
---
+++ someFile.py
@@ -0,0 +1,2 @@
+url = "https://www.github.com/redirection"
+print(url.split("//")[1].split("www.")[1].split(".")[0])
